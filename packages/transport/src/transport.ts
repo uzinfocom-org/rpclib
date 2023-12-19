6fdd972933541f231dff0e38684b2db1435f85a0
stuckintraffic0 <s.qodirjonov@uzinfocom.uz>
2023-12-19 16:33:11 +0500

    fix(transport): messages should come with transport context

--- packages/transport/src/transport.ts
+++ packages/transport/src/transport.ts
@@ -1,11 +1,10 @@
-export type MessageType = Uint8Array | string | object;
-export type OnMessageCallback = (message: MessageType) => void;
+export type OnMessageCallback = (message: Uint8Array, context: any) => void;
 
 export abstract class RPCTransport {
   public readonly abstract name: string;
 
   public abstract onMessage(callback: OnMessageCallback): void;
-  public abstract reply(message: MessageType): Promise<void>;
+  public abstract reply(message: Uint8Array, context: any): Promise<void>;
 
   public abstract launch(): Promise<void>;
   public abstract close(): Promise<void>;
